7774e0bec95a762b9da039afddc661e16ccabc8d
Yagodica <ivantey228@gmail.com>
2022-12-06 07:55:59 +0000

    spizdil

---
+++ projmongflask/app/__init__.py
@@ -0,0 +1,6 @@
+from flask import Flask
+
+# you can choose any name instead app. 
+app = Flask(__name__)
+
+from app import routes
---
+++ projmongflask/docker-entrypoint-flask.sh
@@ -0,0 +1,4 @@
+#!/usr/bin/env bash
+
+echo "Start flask dev server"
+flask run
